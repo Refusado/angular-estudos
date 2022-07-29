fde4e5b9d8dd9f33775722634ac815ae29166318
Refusado <orefusado@gmail.com>
2022-07-29 17:36:01 -0300

    adicionando imagens + novo componente

---
+++ src/app/star/star.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnChanges, SimpleChanges } from "@angular/core";
+
+@Component ({
+    selector: 'app-star',
+    templateUrl: 'start.component.html',
+})
+export class StarComponent implements OnChanges {
+
+    ngOnChanges(changes: SimpleChanges): void {
+         
+    }
+
+}
